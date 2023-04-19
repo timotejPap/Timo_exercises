e989698f1503faee07774a45896fb9146a164f76
papti <paptimotej@gmail.com>
2023-04-19 06:05:57 +0200

    zmeny

---
+++ exercises_with_filter.py
@@ -0,0 +1,12 @@
+
+cisla = [22, 68, -50, 32, -25]
+zaporne = filter(lambda x: x < 0, cisla)
+print(list(zaporne))
+
+slova = ["ryba", "Igor", "macka", "pes", "Stefan"]
+male_pismeno = filter(lambda x: x[0].isupper(), slova)
+print(list(male_pismeno))
+
+udaje = ["Peter", 12, "x", 256, "jablko", 12.9]
+je_cislo = filter(lambda x: isinstance(x, (int, float)), udaje)
+print(list(je_cislo))
