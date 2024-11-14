2f43c974cd6748d7d21f2de3ccfe5d8dd04c6abe
timotejPap <paptimotej@gmail.com>
2024-11-14 19:37:47 +0100

    new course from YT

---
+++ kurz David Šetek/test.py
@@ -0,0 +1,12 @@
+print("Ahoj")
+
+print("Dobrý deň")
+print("Timotej 'Timčo'")
+print("Pap")
+
+# toto je komentár
+# druhý riadok
+
+print("Učím sa Python")
+print("Funkcia print začína slovom print")
+print('Funkciu print zavoláme print ("nejaký text")')
